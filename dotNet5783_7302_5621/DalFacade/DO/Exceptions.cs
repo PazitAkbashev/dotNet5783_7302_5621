05c940e02570284598766242224f2a901943c3c4
Pazit Akbashev <pakbashev@gmail.com>
2022-11-22 17:39:41 +0200

    exeptions file

---
+++ dotNet5783_7302_5621/DalFacade/DO/Exceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO;
+
+int num = int.Parse("aaa");
+public static Exceptin ProductsException(Exception exception)
+{
+    return exception;
+}
+
+public static Exception OrderException(Exception exception)
+{
+    return exception;
+}
+
+public static Exception OrderItemException(Exception exception)
+{
+    return exception;
+}
+
