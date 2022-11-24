ebe7a3a6b7dd2a11813f405408331be3bf464f99
Miriam Zeidman <zeidman@g.jct.ac.il>
2022-11-24 19:40:59 +0200

    working together on the objects

---
+++ dotNet5783_7302_5621/BL/BO/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class Order
+    {
+        public int ID;
+        public string CustomerName;
+        public string CustomerEmail;
+        public string CustomerAdress;
+        public OrderStatus Status;
+        public 
+
+    }
+}
---
+++ dotNet5783_7302_5621/BL/BO/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DO.Enums;
+
+namespace BO
+{
+    public class Product
+    {
+        public int ID;
+        public string Name;
+        public float Price;
+        public Category Category;
+        public int InStock;
+    }
+}
