a0f381fce1d2a059d50231d3e4ac27b056350eb7
Pazit Akbashev <pakbashev@gmail.com>
2022-11-29 12:56:49 +0200

    DN

--- dotNet5783_7302_5621/BL/BO/Order.cs
+++ dotNet5783_7302_5621/BL/BO/Order.cs
@@ -9,17 +9,17 @@ namespace BO
     public class Order
     {
         public int ID;
-        public string CustomerName { get; set; }
-        public string CustomerEmail { get; set; }
-        public string CustomerAddress { get; set; }
+        public string ?CustomerName { get; set; }
+        public string ?CustomerEmail { get; set; }
+        public string ?CustomerAddress { get; set; }
         public OrderStatus Status { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime PaymentDate { get; set; }
         public DateTime ShipDate { get; set; }
         public DateTime DeliveryrDate { get; set; }
-        public OrderItem Items { get; set; }
+        public OrderItem ?Items { get; set; }
         public double TotalPrice { get; set; }
         public override string ToString() => $@"customer-id = {ID},customer-name={CustomerName},Mail={CustomerEmail},Adress= {CustomerAddress},order-status= {Status},
 Order-date={OrderDate}, Payment-date ={PaymentDate},Ship-date={ShipDate},Deliveryr-date={DeliveryrDate},Order-items={Items},total-price={TotalPrice}";
-
+    }
 }
