b3dca7de2e4228b7b4381cfe3890d02f9419ccd9
Miriam Zeidman <zeidman@g.jct.ac.il>
2022-11-29 12:36:31 +0200

    adding back the blapi folder

--- dotNet5783_7302_5621/BL/BlApi/ICart.cs
+++ dotNet5783_7302_5621/BL/BlApi/ICart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace BlApi;
 /// </summary>
 public interface ICart
 {
-    public Cart addProductToCart(Cart,int);
+    public Cart addProductToCart(Cart, int);
     public Cart updateAmountOfProduct(Cart, int, int);
     public void confirmCart(Cart);
 }
