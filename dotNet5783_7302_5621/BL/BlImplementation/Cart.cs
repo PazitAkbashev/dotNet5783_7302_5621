b74f4cdfee772e14922b9705d43262626dc3d3c6
Pazit Akbashev <pakbashev@gmail.com>
2022-11-29 13:43:29 +0200

    opened BLimplementation clases

---
+++ dotNet5783_7302_5621/BL/BlImplementation/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+using DalApi;
+
+namespace BlImplementation;
+
+internal class Cart :ICart
+{
+    private IDal dalCart = new Dal.DalList();
+
+}
---
+++ dotNet5783_7302_5621/BL/BlImplementation/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+using DalApi;
+
+namespace BlImplementation;
+
+internal class Order :IOrder
+{
+   private IDal dalOrder = new Dal.DalList();
+
+}
