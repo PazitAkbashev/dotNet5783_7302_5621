4cf46a3785c463c4cbe80fabc50f339c973590e6
Pazit Akbashev <pakbashev@gmail.com>
2022-12-15 14:51:32 +0200

    8' 9 part 3 changes

--- dotNet5783_7302_5621/PL/Products/ProductListWindow.xaml.cs
+++ dotNet5783_7302_5621/PL/Products/ProductListWindow.xaml.cs
@@ -29,11 +29,14 @@ namespace PL.Products
             InitializeComponent();
             ProductListView.ItemsSource = bl.Product.getProductList();
             CategorySelector.ItemsSource = Enum.GetValues(typeof(BO.Enums.category));
+            ProductListView.ItemsSource = bl.Product.getProductList(); //part 9.3
+
         }
 
         private void Selector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            CategorySelector.SelectedItem.
+            CategorySelector.SelectedItem; //part8
+           
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -41,6 +44,11 @@ namespace PL.Products
 
         }
 
+        private void adminButtonClick(object sender, RoutedEventArgs e)//9.1
+        {
+
+        }
+        
 
     }
 }
