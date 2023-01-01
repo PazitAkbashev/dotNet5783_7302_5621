86d474e95437bd8ba350d1a947e75fc44c6fc166
Miriam Zeidman <zeidman@g.jct.ac.il>
2023-01-01 19:22:20 +0200

    Stage 4 Final commit

--- dotNet5783_7302_5621/BL/BO/Order.cs
+++ dotNet5783_7302_5621/BL/BO/Order.cs
@@ -24,4 +24,5 @@ namespace BO
         public override string ToString() => $@"customer-id = {ID},customer-name={CustomerName},Mail={CustomerEmail},Adress= {CustomerAddress},order-status= {Status},
 Order-date={OrderDate}, Payment-date ={OrderDate},Ship-date={ShipDate},Deliveryr-date={DeliveryrDate},Order-items={Items},total-price={TotalPrice}";
     }
+
 }
